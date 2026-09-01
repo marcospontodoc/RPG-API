75a0dcf98ef12828a07f5a10117ddb16521dabcd
Marcos <marcospontodoc@gmail.com>
2026-09-01 10:39:10 -0300

    Add initial project structure with Maven configuration and Spring Boot setup

---
+++ rpg-api/src/test/java/com/ragnarok/rpg_api/RpgApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ragnarok.rpg_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
