665fc5fc4245f6397afd58d02d563aa9c8901384
Marcos <marcospontodoc@gmail.com>
2026-09-04 10:59:27 -0300

    Add CORS configuration to allow requests from Swagger UI

---
+++ rpg-api/src/main/java/com/ragnarok/rpg_api/Config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.ragnarok.rpg_api.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**")
+                        .allowedOrigins("https://editor.swagger.io")
+                        .allowedMethods(
+                                "GET",
+                                "POST",
+                                "PUT",
+                                "DELETE",
+                                "OPTIONS"
+                        );
+            }
+        };
+    }
+}
